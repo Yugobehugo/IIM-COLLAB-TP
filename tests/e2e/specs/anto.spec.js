767f479af4aae4948300e640aeeaad37c1b12222
Hugo Boudalier <70762022+Yugobehugo@users.noreply.github.com>
2021-09-22 17:56:09 +0200

    Update anto.spec.js

--- tests/e2e/specs/anto.spec.js
+++ tests/e2e/specs/anto.spec.js
@@ -1,19 +1,19 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("Scenarios for page Antonio", () => {
-  it("Visits page /antonio", () => {
-    cy.visit("http://localhost:8081/antonio");
-    cy.get('[href="/antonio"]').click();
-    cy.url().should("include", "/antonio");
-  });
+    it("Visits page /antonio", () => {
+        cy.visit("http://localhost:8080/");
+        cy.get('[href="/antonio"]').click();
+        cy.url().should("include", "/antonio");
+    });
 
-  it("Should change project when the shuffle button is clicked", () => {
-    cy.visit("http://localhost:8081/antonio");
+    it("Should change project when the shuffle button is clicked", () => {
+        cy.visit("http://localhost:8080/antonio");
 
-    cy.get(".desc > a").should("have.text", "A Monthly spotify recap");
+        cy.get(".desc > a").should("have.text", "A Monthly spotify recap");
 
-    cy.get(".shuffle").click();
+        cy.get(".shuffle").click();
 
-    cy.get(".desc > a").should("not", "have.text", "A Monthly spotify recap");
-  });
+        cy.get(".desc > a").should("not", "have.text", "A Monthly spotify recap");
+    });
 });
