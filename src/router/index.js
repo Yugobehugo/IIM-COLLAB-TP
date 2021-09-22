e65696a69bcde5d18bbf5e223c4b64618eeb091b
AhmedDWIIM <ahmedokc@gmail.com>
2021-09-22 15:08:40 +0200

    APP + INDEX commit2

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     name: "Antonio",
     component: () => import("../views/Antonio.vue"),
   },
+  {
+    path: "/ahmed",
+    name: "ahmed",
+    component: () => import("../views/Ahmed.vue"),
+  },
 ];
 
 const router = createRouter({
